78fc03dac445a866245d28c9aa2f70c4915e0f10
Jose Bedoya <jossephb@gmail.com>
2019-01-07 10:16:38 -0500

    User passport

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: 'mongodb://jose:jose1234@ds161312.mlab.com:61312/devconnudemy'
+  mongoURI: 'mongodb://jose:jose1234@ds161312.mlab.com:61312/devconnudemy',
+  secretOrKey: 'secret'
 }
