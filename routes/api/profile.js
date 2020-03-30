a19cd6be33b5cf0f287112369262378d63bac03b
Jose Bedoya <jossephb@gmail.com>
2020-03-29 19:19:37 -0500

    get all profiles and get profile by user id

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -39,7 +39,7 @@ router.post('/', [ auth,
     check('skills', 'Skills is required').not().isEmpty()
   ]
 ],
-async(req, res) => {
+async (req, res) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(400).json({ errors: errors.array() });
@@ -106,58 +106,40 @@ async(req, res) => {
 });
 
 
+// @route   GET api/profile
+// @desc    Get all profiles
+// @access  Public
+router.get('/', async (req, res) => {
+  try {
+    const profiles = await Profile.find().populate('user', ['name', 'avatar']);
+    res.json(profiles);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+// @route   GET api/profile/user/:user_id
+// @desc    Get profile by user ID
+// @access  Public
+router.get('/user/:user_id', async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.params.user_id
+    }).populate('user', ['name', 'avatar']);
+
+    if (!profile)
+      return res.status(400).json({ msg: 'Profile not found' });
+
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind === 'ObjectId') {
+      return res.status(400).json({ msg: 'Profile not found' });
+    }
+    res.status(500).send('Server Error');
+  }
+});
 
 
-
-
-// @route   POST api/profile
-// @desc    Create user profile
-// @access  Private
-// router.post(
-//   '/',
-//   passport.authenticate('jwt', { session: false }),
-//   (req, res) => {
-//     // Get fields
-//     const profileFields = {}
-//     if (req.body.handle) profileFields.handle = req.body.handle;
-//     if (req.body.company) profileFields.company = req.body.company;
-//     if (req.body.website) profileFields.website = req.body.website;
-//     if (req.body.location) profileFields.location = req.body.location;
-//     if (req.body.status) profileFields.status = req.body.status;
-//     if (req.body.bio) profileFields.bio = req.body.bio;
-//     if (req.body.githubusername) profileFields.githubusername = req.body.githubusername;
-//     // Skills - split into array
-//     if (typeof req.body.skills !== 'undefined') {
-//       profileFields.skills = req.body.skills.split(',');
-//     }
-//     // Social
-//     profileFields.social = {};
-//     if (req.body.youtube) profileFields.social.youtube = req.body.youtube;
-//     if (req.body.twitter) profileFields.social.twitter = req.body.twitter;
-//     if (req.body.facebook) profileFields.social.facebook = req.body.facebook;
-//     if (req.body.linkedin) profileFields.social.linkedin = req.body.linkedin;
-//     if (req.body.instagram) profileFields.social.instagram = req.body.instagram;
-
-//     Profile.findOne({ user: req.user.id }).then(profile => {
-//       if (profile) {
-//         // Update
-//         Profile.findOneAndUpdate(
-//           { user: req.user.id },
-//           { $set: profileFields },
-//           { new: true }
-//         ).then(profile => res.json(profile))
-//       } else {
-//         // Create
-//         // Check if handle exists
-//         Profile.findOne({ handle: profileFields.handle }).then(profile => {
-//           if (profile) {
-//             errors.handle = 'That handle already exists';
-//             res.status(400).json(errors);
-//           }
-//         });
-//       }
-//     });
-//   }
-// );
-
-module.exports = router
+module.exports = router;
--- server.js
+++ server.js
@@ -1,9 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
 
-// const mongoose = require('mongoose');
-// const passport = require('passport');
-
 const app = express();
 
 // Connect to MongoDB
@@ -16,14 +13,6 @@ const auth = require('./routes/api/auth');
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
 const posts = require('./routes/api/posts');
-
-
-
-// Passport middleware
-// app.use(passport.initialize());
-
-// // Passport Config
-// require('./config/passport')(passport);
 
 // Define routes
 app.use('/api/auth', auth);
